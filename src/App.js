2e6ae6786d224d61ced761f398599a16bb448ea8
pranshu <srivastavapranshu2@gmail.com>
2023-03-23 23:40:45 +0530

    added route for login and signup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import CreateBlog from "./components/CreateBlog";
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
 import SharedLayout from "./components/SharedLayout";
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
             <Route index element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/create" element={<CreateBlog />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Signup" element={<Signup />} />
           </Route>
         </Routes>
       </BrowserRouter>
