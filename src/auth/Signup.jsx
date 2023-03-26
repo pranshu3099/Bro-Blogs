6008f17efbeca39d0c90c555abeff784a0c0dcd3
pranshu <srivastavapranshu2@gmail.com>
2023-03-26 21:22:59 +0530

    added signup page

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,8 +1,67 @@
+import { Input, Button } from "@chakra-ui/react";
+import user from "/media/pranshu/My Passport/my-blog/src/icons/user.png";
+import email from "/media/pranshu/My Passport/my-blog/src/icons/email.png";
+import phone from "/media/pranshu/My Passport/my-blog/src/icons/phone.png";
+import eye from "/media/pranshu/My Passport/my-blog/src/icons/eye.png";
+
 const Signup = () => {
   return (
-    <div>
-      <h1>Signup</h1>
-    </div>
+    <>
+      <div className="signup">
+        <h1 className="signup-h1">
+          Hey there!! Welcome to Bro Blogs sign-up and start writing your blog
+        </h1>
+      </div>
+      <form action="">
+        <div className="signup-container">
+          <div>
+            <Input
+              type="text"
+              placeholder="Your name"
+              width={"500px"}
+              margin={5}
+              variant="flushed"
+            />
+            <img className="sign-up-icons" src={user} alt="" />
+            <Input
+              type="number"
+              placeholder="Mobile number"
+              width={"500px"}
+              margin={5}
+              variant="flushed"
+            />
+            <img className="sign-up-icons" src={phone} alt="" />
+            <Input
+              type="email"
+              placeholder="email"
+              width={"500px"}
+              margin={5}
+              variant="flushed"
+            />
+            <img className="sign-up-icons" src={email} alt="" />
+            <Input
+              type="Password"
+              placeholder="password"
+              width={"500px"}
+              margin={5}
+              variant="flushed"
+            />
+            <img className="sign-up-icons" src={eye} alt="" />
+            <Input
+              type="Password"
+              placeholder="Reenter the password"
+              width={"500px"}
+              margin={5}
+              variant="flushed"
+            />{" "}
+            <img className="sign-up-icons" src={eye} alt="" />
+            <Button colorScheme="blue" mr={3}>
+              Login
+            </Button>
+          </div>
+        </div>
+      </form>
+    </>
   );
 };
 
