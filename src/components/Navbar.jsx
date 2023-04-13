19f36340150c8ce3142d5d0db8aac806e9f03eec
pranshu <srivastavapranshu2@gmail.com>
2023-04-14 00:30:05 +0530

    displayed navbar according to user authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,46 @@
 import { Link } from "react-router-dom";
 import Search from "./Search";
+import { useAuthContext } from "../context/Provider";
+import Profile from "./Profile";
 const Navbar = () => {
+  const { auth, responseData } = useAuthContext();
   return (
     <nav className="container">
       <div>
         <h1 className="logo">Bro Blogs</h1>
       </div>
-      <Search />
-      <div className="nav-icons">
-        <Link to="/" className="icons">
-          Home
-        </Link>
-        <Link to="/about" className="icons">
-          About
-        </Link>
-        <Link to="/create" className="icons">
-          Write Blog
-        </Link>
-        <Link to="/login" className="icons">
-          Login
-        </Link>
-        <Link to="/signup" className="icons">
-          Signup
-        </Link>
-      </div>
+      <>
+        {auth && (
+          <div className="nav-main-container">
+            <Search />
+            <div className="nav-icons">
+              <Link to="/" className="icons">
+                Home
+              </Link>
+              <Link to="/about" className="icons">
+                About
+              </Link>
+              <Link to="/create" className="icons">
+                Write Blog
+              </Link>
+              <Profile />
+            </div>
+          </div>
+        )}
+        <div className="nav-icons">
+          {!auth && (
+            <>
+              {" "}
+              <Link to="/login" className="icons">
+                Login
+              </Link>
+              <Link to="/signup" className="icons">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </>
     </nav>
   );
 };
