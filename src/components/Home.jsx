eff0b818906ffba30d8997b1d020e3135e15196d
pranshu <srivastavapranshu2@gmail.com>
2023-04-16 23:36:38 +0530

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,33 @@
 const Home = () => {
-    return ( 
-        <div>
-            <h1>Home</h1>
+  return (
+    <article>
+      <div className="post-main-container">
+        <div className="post-container">
+          <div className="date-name-container">
+            <p>Pranshu Srivastava</p>
+            <p>15-april-2023</p>
+          </div>
+          <div className="post-title">
+            <h1>My first blog on Artificial Intelligence</h1>
+          </div>
+          <div>
+            <p>
+              One reason for the stir delights me and makes me incredibly happy
+              as a teacher of mathematics. By all accounts, these two teenage
+              math students are the exact opposite of the majority of the math
+              establishment. They are female, they are African-American, and
+              they come from an area which is not particularly renowned for
+              producing high academic achievers. This is just an awesome turn of
+              events and one which should inspire anyone — no matter what their
+              gender, ethnic or socio-demographic background — that excellence
+              in your chosen field of study is always attainable if you have
+              enough joy and passion for what you do.
+            </p>
+          </div>
         </div>
-     );
-}
- 
+      </div>
+    </article>
+  );
+};
+
 export default Home;
