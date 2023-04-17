3c0235c9771c7cfb0a9080f4b674d802975fbf33
pranshu <srivastavapranshu2@gmail.com>
2023-04-18 00:55:48 +0530

    added like and comment icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,21 @@
+import like from "/media/pranshu/My Passport/my-blog/src/icons/like.png";
+import comment from "/media/pranshu/My Passport/my-blog/src/icons/comment.png";
+import heart from "/media/pranshu/My Passport/my-blog/src/icons/heart.png";
+import { useState } from "react";
+
 const Home = () => {
+  let changeIcon = (e, icon) => {
+    if (icon === like) {
+      e.target.attributes.src.textContent = heart;
+    } else {
+      e.target.attributes.src.textContent = like;
+    }
+  };
+  const handleLike = (e) => {
+    const icon = e.target.attributes.src.textContent;
+    changeIcon(e, icon);
+  };
+
   return (
     <article>
       <div className="post-main-container">
@@ -21,10 +38,45 @@ const Home = () => {
               events and one which should inspire anyone — no matter what their
               gender, ethnic or socio-demographic background — that excellence
               in your chosen field of study is always attainable if you have
-              enough joy and passion for what you do.
+              enough joy and passion for what you do. One reason for the stir
+              delights me and makes me incredibly happy as a teacher of
+              mathematics. By all accounts, these two teenage math students are
+              the exact opposite of the majority of the math establishment. They
+              are female, they are African-American, and they come from an area
+              which is not particularly renowned for producing high academic
+              achievers. This is just an awesome turn of events and one which
+              should inspire anyone — no matter what their gender, ethnic or
+              socio-demographic background — that excellence in your chosen
+              field of study is always attainable if you have enough joy and
+              passion for what you do. One reason for the stir delights me and
+              makes me incredibly happy as a teacher of mathematics. By all
+              accounts, these two teenage math students are the exact opposite
+              of the majority of the math establishment. They are female, they
+              are African-American, and they come from an area which is not
+              particularly renowned for producing high academic achievers. This
+              is just an awesome turn of events and one which should inspire
+              anyone — no matter what their gender, ethnic or socio-demographic
+              background — that excellence in your chosen field of study is
+              always attainable if you have enough joy and passion for what you
+              do.
             </p>
           </div>
+          <div className="post-icons-container">
+            {
+              <img
+                src={like}
+                alt=""
+                className="post-icons"
+                onClick={handleLike}
+                id="likes"
+              />
+            }
+            <p>50k</p>
+            <img src={comment} alt="" className="post-icons" />
+            <p>34</p>
+          </div>
         </div>
+        <div></div>
       </div>
     </article>
   );
