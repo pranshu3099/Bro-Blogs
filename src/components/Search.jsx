45c972b9d9595a2e6ddec33dd314aa48fcf5d2e3
pranshu <srivastavapranshu2@gmail.com>
2023-04-18 00:56:25 +0530

    added ctrl+k functionality for search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
+import { Input } from "@chakra-ui/react";
 const Search = () => {
-    const [searchBlog, setSearchBlog] = useState('');
-    return ( <>
-    
-    <div>
-        <input type="text" placeholder="search blogs press ctr+k" value={searchBlog} onChange = {e=>setSearchBlog(e.target.value)}/>
-    </div>
-        
-    </> 
-    );
-}
- 
+  const [searchBlog, setSearchBlog] = useState("");
+  const searchbox = document.getElementById("search");
+  document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.key === "k") {
+      e.preventDefault();
+      searchbox.focus();
+    } else if (e.key === "Escape") {
+      searchbox.blur();
+    }
+  });
+  return (
+    <>
+      <div className="search">
+        <Input
+          variant="filled"
+          type="text"
+          placeholder="search technocrats press ctr+k"
+          value={searchBlog}
+          id="search"
+          onChange={(e) => setSearchBlog(e.target.value)}
+        />
+      </div>
+    </>
+  );
+};
+
 export default Search;
