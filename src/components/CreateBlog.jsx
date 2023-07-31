7bd3e52ba37e858332dd087fd5081de210809539
pranshu <srivastavapranshu2@gmail.com>
2023-08-01 01:31:18 +0530

    create blog using markdown

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -6,6 +6,8 @@ import axios from "axios";
 import Home from "./Home";
 import FileResizer from "react-image-file-resizer";
 import EditorComponent from "./Editor";
+import url from "/home/pranshu/Bro Blogs/Bro-Blogs/src/icons/url.png";
+
 import {
   Modal,
   ModalOverlay,
@@ -21,14 +23,16 @@ import {
   Textarea,
   Select,
 } from "@chakra-ui/react";
+import { upload } from "@testing-library/user-event/dist/upload";
 const CreateBlog = () => {
   const getBearerToken = () => localStorage.getItem("Bearer");
   const [bearer] = useState(getBearerToken);
   const [data, setData] = useState("");
   const [selectedFile, setSelectedFile] = useState([]);
+  const [imagePreview, setImagePreview] = useState([]);
   const [resizedImage, setResizedImage] = useState(null);
   const [additionalComponents, setAdditionalComponents] = useState([]);
-  let [id, setId] = useState(0);
+  const [url, setUrl] = useState([]);
   const headers = useMemo(
     () => ({
       Authorization: `${bearer}`,
@@ -54,68 +58,132 @@ const CreateBlog = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
   const { responseData } = useAuthContext();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const blogReducer = (state, action) => {
-    switch (action.type) {
+
+  const blogReducer = (state, { type, payload }) => {
+    switch (type) {
       case "pending":
-        return action;
+        return state;
       case "title":
-        return { ...state, title: action.title };
+        return { ...state, ...payload };
       case "content":
-        return { ...state, yourblog: action.yourblog };
+        return { ...state, ...payload };
       case "resolved":
-        return action;
+        return state;
+      default:
+        throw new Error("error");
     }
   };
+
   const [blog, dispatch] = useReducer(blogReducer, {
     title: "",
-    yourblog: "",
-    type: "pending",
+    blogContent: "",
   });
+
   const [submit, setSubmit] = useState(false);
   const today = new Date();
   const monthOptions = { month: "long" };
-
+  // console.log(blog);
   const handleSubmit = (e) => {
+    console.log(blog);
     e.preventDefault();
-    if (blog.title === "" && blog.yourblog === "") {
+    if (blog.title === "" && blog.blogContent === "") {
       alert("please write your blog");
     }
-    dispatch({ title: "", yourblog: "", type: "resolved" });
+    // dispatch({ title: "", blogContent: "", type: "resolved" });
     let randomNum = Math.floor(Math.random() * 9000) + 1000;
     let data = {
       title: blog?.title,
-      content: blog?.yourblog,
+      content: blog?.blogContent,
       user_id: Number(responseData?.data?.user?.id),
       category_id: Number(selectedCategory),
       posts_id: Number(randomNum),
-      // image: resizedImage,
+      image: selectedFile,
     };
-    console.log(data);
-    // axios
-    //   .post("http://127.0.0.1:3000/createposts", data, { headers })
-    //   .then((response) => {
-    //     if (response.status === 200) {
-    //       setSubmit(true);
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+    axios
+      .post("http://127.0.0.1:3000/createposts", data, { headers })
+      .then((response) => {
+        if (response.status === 200) {
+          setSubmit(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
+
   const handleCategoryChange = (value) => {
     setSelectedCategory(value);
   };
-  const handleRemovelist = (url) => {
-    let newfilearr = selectedFile.filter((img) => {
+  const handleRemovelist = (url, list_img) => {
+    console.log(list_img);
+    let newfilearr = imagePreview.filter((img) => {
       if (img !== url) return true;
       return false;
     });
-    setSelectedFile(newfilearr);
+    setImagePreview(newfilearr);
+    let newimagearr = selectedFile.filter((img) => {
+      if (img !== list_img) return true;
+      return false;
+    });
+    setSelectedFile(newimagearr);
+  };
+
+  const handleContentChange = (state, action) => {
+    blogReducer(state, action);
   };
 
   const handleFileInputChange = (e) => {
     const file = e.target.files[0];
     setSelectedFile([...selectedFile, file.name]);
+    let reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview([...imagePreview, reader.result]);
+    };
+
+    if (file) reader.readAsDataURL(file);
+  };
+
+  const copyTextToClipboard = (text) => {
+    if (navigator.clipboard && window.isSecureContext) {
+      // Use Clipboard API if available
+      navigator.clipboard.writeText(text);
+    } else {
+      const textarea = document.createElement("textarea");
+      textarea.value = text;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textarea);
+    }
+  };
+
+  const handleGeturl = (e) => {
+    let textToCopy = e.target.previousElementSibling.textContent;
+    copyTextToClipboard(textToCopy);
+  };
+
+  const uploadImages = () => {
+    const formData = new FormData();
+    selectedFile.forEach((image) => {
+      formData.append("images", image);
+    });
+    try {
+      const response = axios
+        .post("http://127.0.0.1:3000/api/uploadimage", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `${bearer}`,
+          },
+        })
+        .then((res) => {
+          setUrl(res.data.url);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -139,7 +207,10 @@ const CreateBlog = () => {
                   className="input"
                   placeholder="Title"
                   onChange={(e) =>
-                    dispatch({ ...blog, title: e.target.value, type: "title" })
+                    dispatch({
+                      type: "title",
+                      payload: { title: e.target.value },
+                    })
                   }
                 />
               </label>
@@ -150,6 +221,7 @@ const CreateBlog = () => {
                 accept="image/*"
                 id="files"
                 onChange={handleFileInputChange}
+                name="images"
               />
             </div>
             <div>
@@ -157,17 +229,22 @@ const CreateBlog = () => {
                 <ImageUrllist
                   list={selectedFile}
                   removelist={handleRemovelist}
+                  imgurl={handleGeturl}
+                  preview={imagePreview}
+                  copyurl={url}
                 />
               }
             </div>
-
+            <div>
+              <button onClick={uploadImages}>upload images</button>
+            </div>
             <div>
               <div className="blog-sub-container">
                 <div>
                   <EditorComponent
-                    reducer={blogReducer}
                     blog={blog}
                     dispatch={dispatch}
+                    handleChange={handleContentChange}
                   />
                 </div>
               </div>
@@ -201,7 +278,7 @@ const CreateBlog = () => {
                       " " +
                       today.getFullYear()}
                   </p>
-                  <Text fontSize="15px">{blog?.yourblog}</Text>
+                  <Text fontSize="15px">{blog?.blogContent}</Text>
                 </ModalBody>
 
                 <ModalFooter>
@@ -223,22 +300,26 @@ const CreateBlog = () => {
   );
 };
 
-const ImageUrllist = ({ list, removelist }) => {
-  console.log(list);
-  const handleRemoveurl = (url) => {
-    removelist(url);
+const ImageUrllist = ({ list, removelist, preview, imgurl, copyurl }) => {
+  const handleRemoveurl = (img, list_img) => {
+    removelist(img, list_img);
   };
+
+  const handleurl = (e) => {
+    imgurl(e);
+  };
+
   return (
     <ul className="imgparent">
-      {list.map((url, index) => {
+      {preview.map((img, index) => {
         return (
           <>
             <li key={index} value={index} className="imglist">
-              {url}
+              <img src={img} alt="img" />
             </li>
             <span
               onClick={(e) => {
-                handleRemoveurl(url);
+                handleRemoveurl(img, list[index]);
               }}
               style={{
                 cursor: "pointer",
@@ -247,6 +328,22 @@ const ImageUrllist = ({ list, removelist }) => {
             >
               x
             </span>
+            <p style={{ display: "none" }} value={index}>
+              {copyurl[index]?.path}
+            </p>
+            <img
+              src={url}
+              alt=""
+              style={{
+                cursor: "pointer",
+                width: "17px",
+                margin: "6px",
+              }}
+              id="imgurl"
+              onClick={(e) => {
+                handleurl(e);
+              }}
+            />
           </>
         );
       })}
