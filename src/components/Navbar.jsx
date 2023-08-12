4083202c8a4661e5b4643e15549e252e13ad9b5d
pranshu <srivastavapranshu2@gmail.com>
2023-08-12 23:12:38 +0530

    only Home write blog in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,6 @@ const Navbar = () => {
   const { auth, responseData } = useAuthContext();
   return (
     <nav className="container">
-      <div>
-        <h1 className="logo">Bro Blogs</h1>
-      </div>
       <>
         {auth && (
           <div className="nav-main-container">
@@ -19,31 +16,10 @@ const Navbar = () => {
               <Link to="/create" className="icons">
                 Write Blog
               </Link>
-
               <Profile />
-              <Search />
             </div>
           </div>
         )}
-        <div className="nav-icons">
-          {!auth && (
-            <>
-              {" "}
-              <Link to="/" className="icons">
-                Home
-              </Link>
-              <Link to="/create" className="icons">
-                Write Blog
-              </Link>
-              <Link to="/login" className="icons">
-                Login
-              </Link>
-              <Link to="/signup" className="icons">
-                Signup
-              </Link>
-            </>
-          )}
-        </div>
       </>
     </nav>
   );
