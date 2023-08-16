6e53ff0487b183d70171e9f5217b44b71a1b6786
pranshu <srivastavapranshu2@gmail.com>
2023-08-16 17:05:23 +0530

    check for authentication when admin logins

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,21 @@ import { Link } from "react-router-dom";
 import Search from "./Search";
 import { useAuthContext } from "../context/Provider";
 import Profile from "./Profile";
+import { useState } from "react";
 const Navbar = () => {
   const { auth, responseData } = useAuthContext();
+  const getBearerToken = () => localStorage.getItem("Bearer");
+  const [bearer] = useState(getBearerToken);
+  let verified;
+  if (bearer !== null) {
+    verified = true;
+  } else {
+    verified = auth;
+  }
   return (
     <nav className="container">
       <>
-        {auth && (
+        {verified && (
           <div className="nav-main-container">
             <div className="nav-icons">
               <Link to="/" className="icons">
